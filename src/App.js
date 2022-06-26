e07bdbc12d005cb75346a3effb86300fd7677c2d
Arun3sh <m.aruneshwar@gmail.com>
2022-06-26 23:16:52 +0530

    page route added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { Route, Switch } from "react-router-dom";
+import TodoPage from "./pages/TodoPage/TodoPage";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <TodoPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
