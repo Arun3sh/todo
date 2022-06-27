ace5c8f1c2565f9fb0d88e843a656cfe3aea004d
Arun3sh <m.aruneshwar@gmail.com>
2022-06-27 23:57:53 +0530

    header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header-container container-sm">
+      <div className="header-wrapper"></div>
+    </div>
+  );
+}
+
+export default Header;
