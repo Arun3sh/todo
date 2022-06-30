6cd0292ed981e1d4859640470dc63bb23bcf64c8
Arun3sh <m.aruneshwar@gmail.com>
2022-06-30 22:51:06 +0530

    header modified

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,9 @@ import "./Header.css";
 function Header() {
   return (
     <div className="header-container container-sm">
-      <div className="header-wrapper"></div>
+      <div className="header-wrapper">
+        <h1 className="title">TODO</h1>
+      </div>
     </div>
   );
 }
